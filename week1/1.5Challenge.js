cc9a557785cba388d1c1bbef9001bc44eb2aceba
KaanCaman <106434723+KaanCaman@users.noreply.github.com>
2025-02-13 17:32:34 +0300

    The challenge was accepted and won. 💪 | Meydan okuma kabul edilip kazanıldı. 💪

---
+++ week1/1.5Challenge.js
@@ -0,0 +1,21 @@
+// Specify which of the examples are true or false.
+
+// Örneklerin hangilerinin true veya false olduğunu belirtin.
+
+/* 1 */ console.log(5 == "5");
+/* 2 */ console.log(null == undefined);
+/* 3 */ console.log(0 == false);
+
+/* A */ console.log(5 === "5");
+/* B */ console.log(null === undefined);
+/* C */ console.log(0 === false);
+
+/*
+1 -> true
+2 -> true
+3 -> true
+
+A -> false
+B -> false
+C -> false
+*/
